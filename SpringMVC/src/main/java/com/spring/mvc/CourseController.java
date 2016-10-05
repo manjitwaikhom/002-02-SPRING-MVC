80b06b4854f6bed3af9b01dbfeca2210f1fc0035
manjitwaikhom <manjitwaikhom@gmail.com>
2016-10-05 23:05:32 +0530

    pilot

---
+++ SpringMVC/src/main/java/com/spring/mvc/CourseController.java
@@ -0,0 +1,28 @@
+package com.spring.mvc;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+@Controller
+public class CourseController{
+	
+	@Autowired
+	private CourseService cs;
+
+	@RequestMapping(value="/course.jlc")
+	protected String getCourses(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ArrayList<String> al=cs.getCourses();
+		request.setAttribute("AL", al);
+		return "show";
+	}
+
+}
---
+++ SpringMVC/src/main/webapp/show.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="jlc" uri="http://java.sun.com/jsp/jstl/core"  %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>show courses</title>
+<h1>Course Information</h1>
+</head>
+<body>
+<ul><jlc:forEach var="cou" items="${AL}">
+<li><h2>${cou}</h2></li>
+
+</jlc:forEach>
+</ul>
+</body>
+</html>
